967dcefafa79721e8319a55c4935c82658a92d10
AxeKam333 <olek.kaminski5@gmail.com>
2023-11-06 08:38:49 +0100

    corrected reading from file

--- zachlanne/cartesian.cpp
+++ zachlanne/cartesian.cpp
@@ -6,33 +6,38 @@
 
 using namespace std;
 
-
-struct Point {
-    int x, y;
+struct Point
+{
+    int id, x, y;
 };
 
-double calculateDistance(const Point& p1, const Point& p2) {
+double calculateDistance(const Point &p1, const Point &p2)
+{
     int dx = p1.x - p2.x;
     int dy = p1.y - p2.y;
     return std::sqrt(dx * dx + dy * dy);
 }
 
-vector<Point> greedyTSP(vector<Point> points) {
+vector<Point> greedyTSP(vector<Point> points)
+{
     vector<Point> tour;
 
     // Start with the first point as the initial city
     tour.push_back(points[0]);
     points.erase(points.begin());
 
-    while (!points.empty()) {
+    while (!points.empty())
+    {
         Point currentCity = tour.back();
         double minDistance = calculateDistance(currentCity, points[0]);
         int closestCityIndex = 0;
 
         // Find the closest unvisited city
-        for (int i = 1; i < points.size(); i++) {
+        for (int i = 1; i < points.size(); i++)
+        {
             double distance = calculateDistance(currentCity, points[i]);
-            if (distance < minDistance) {
+            if (distance < minDistance)
+            {
                 minDistance = distance;
                 closestCityIndex = i;
             }
@@ -49,19 +54,26 @@ vector<Point> greedyTSP(vector<Point> points) {
     return tour;
 }
 
-int main() {
+int main()
+{
     vector<Point> cities;
 
     // Read cities from a file
     ifstream inputFile("plik.txt");
-    if (!inputFile.is_open()) {
+    if (!inputFile.is_open())
+    {
         cerr << "Failed to open the file." << endl;
         return 1;
     }
 
+    int n, s;
+    inputFile >> n;
+
     int x, y;
-    while (inputFile >> x >> y) {
-        Point point = {x,y};
+    for (int i = 1; i <= n; i++)
+    {
+        inputFile >> s >> x >> y;
+        Point point = {s, x, y};
         cities.push_back(point);
     }
     inputFile.close();
@@ -69,10 +81,18 @@ int main() {
     vector<Point> tour = greedyTSP(cities);
 
     cout << "trasa: ";
-    for (const Point& city : tour) {
+    for (const Point &city : tour)
+    {
         cout << "(" << city.x << ", " << city.y << ") -> ";
     }
     cout << "(" << tour[0].x << ", " << tour[0].y << ")" << endl;
+
+    cout << "trasa2: ";
+    for (const Point &city : tour)
+    {
+        cout << "(" << city.id << ") -> ";
+    }
+    cout << "(" << tour[0].id << ")" << endl;
 
     return 0;
 }
