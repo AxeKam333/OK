7476ef08588ec5b28cda53161bf0bd1b3f640431
AxeKam333 <olek.kaminski5@gmail.com>
2023-12-04 09:32:39 +0100

    testy

--- wyzarzanie.cpp
+++ wyzarzanie.cpp
@@ -10,40 +10,43 @@
 
 using namespace std;
 
-struct City {
+struct City
+{
     string name;
     int x, y;
 };
 
-// Function to calculate the Euclidean distance between two cities
-double calculateDistance(const City& a, const City& b) {
+double calculateDistance(const City &a, const City &b)
+{
     return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 
-// Function to calculate the total distance of a tour
-double calculateTotalDistance(const vector<int>& tour, const vector<City>& cities) {
+double calculateTotalDistance(const vector<int> &tour, const vector<City> &cities)
+{
     double distance = 0.0;
     int n = tour.size();
-    for (int i = 0; i < n - 1; ++i) {
+    for (int i = 0; i < n - 1; ++i)
+    {
         distance += calculateDistance(cities[tour[i]], cities[tour[i + 1]]);
     }
     distance += calculateDistance(cities[tour[n - 1]], cities[tour[0]]); // Return to the starting city
     return distance;
 }
 
-// Function to generate a random initial tour
-vector<int> generateRandomTour(int n) {
+vector<int> generateRandomTour(int n)
+{
     vector<int> tour(n);
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
+    {
         tour[i] = i;
     }
     shuffle(tour.begin() + 1, tour.end(), std::default_random_engine(std::random_device()()));
     return tour;
 }
 
-// Function to perform Simulated Annealing
-vector<int> simulatedAnnealing(const vector<City>& cities, double initialTemperature,
-                               double coolingRate, int maxIterations) {
+vector<int> simulatedAnnealing(const vector<City> &cities, double initialTemperature,
+                               double coolingRate, int maxIterations)
+{
     srand(time(nullptr));
 
     int n = cities.size();
@@ -53,25 +56,25 @@ vector<int> simulatedAnnealing(const vector<City>& cities, double initialTempera
     double currentDistance = calculateTotalDistance(currentTour, cities);
     double bestDistance = currentDistance;
 
-    for (int iteration = 0; iteration < maxIterations; ++iteration) {
+    for (int iteration = 0; iteration < maxIterations; ++iteration)
+    {
         double temperature = initialTemperature * exp(-coolingRate * iteration);
 
-        // Generate a new tour by swapping two cities
         vector<int> newTour = currentTour;
-        int pos1 = rand() % (n - 1) + 1; // Avoid swapping the starting city
+        int pos1 = rand() % (n - 1) + 1; 
         int pos2 = rand() % (n - 1) + 1;
         swap(newTour[pos1], newTour[pos2]);
 
-        double newDistance = calculateTotalDistance(newTour, cities);
+        double newDistance = calculateTotalDistance(newTour, cities); //policzyć zmienione krawędzie, zamiast od nowa
 
-        // Accept the new tour if it is better or with a certain probability if it is worse
-        if (newDistance < currentDistance || (rand() / (RAND_MAX + 1.0)) < exp((currentDistance - newDistance) / temperature)) {
+        if (newDistance < currentDistance || (rand() / (RAND_MAX + 1.0)) < exp((currentDistance - newDistance) / temperature))
+        {
             currentTour = newTour;
             currentDistance = newDistance;
         }
 
-        // Update the best tour if necessary
-        if (currentDistance < bestDistance) {
+        if (currentDistance < bestDistance)
+        {
             bestTour = currentTour;
             bestDistance = currentDistance;
         }
@@ -80,44 +83,51 @@ vector<int> simulatedAnnealing(const vector<City>& cities, double initialTempera
     return bestTour;
 }
 
-// Function to read cities from a file
-vector<City> readCitiesFromFile(const string& filename) {
+vector<City> readCitiesFromFile(const string &filename)
+{
     vector<City> cities;
     ifstream file(filename);
-    if (file.is_open()) {
+    if (file.is_open())
+    {
         int numCities;
         file >> numCities;
 
-        for (int i = 0; i < numCities; ++i) {
+        for (int i = 0; i < numCities; ++i)
+        {
             City city;
             file >> city.name >> city.x >> city.y;
             cities.push_back(city);
         }
 
         file.close();
-    } else {
+    }
+    else
+    {
         cerr << "Unable to open file: " << filename << endl;
     }
     return cities;
 }
 
-int main() {
-    // Example file format: "cities.txt" with the number of cities and city information
-    vector<City> cities = readCitiesFromFile("cities.txt");
+int main(int argc, char *argsv[])
+{
+
+    string name = "cities";
+    if (argc > 1)
+        name = argsv[1];
+
+    vector<City> cities = readCitiesFromFile("./tests/"+name+".txt");
 
     int numCities = cities.size();
 
-    // Set parameters for Simulated Annealing
-    double initialTemperature = 1000.0;
-    double coolingRate = 0.003;
-    int maxIterations = 10000;
+    double initialTemperature = 100000.0;
+    double coolingRate = 0.00003;
+    int maxIterations = 10000000;
 
-    // Find the best tour using Simulated Annealing
     vector<int> bestTour = simulatedAnnealing(cities, initialTemperature, coolingRate, maxIterations);
 
-    // Output the results
     cout << "Best Tour: ";
-    for (int cityIndex : bestTour) {
+    for (int cityIndex : bestTour)
+    {
         cout << cities[cityIndex].name << " ";
     }
     cout << "\nBest Distance: " << calculateTotalDistance(bestTour, cities) << endl;
--- zachlanne/cartesian.cpp
+++ zachlanne/cartesian.cpp
@@ -3,40 +3,50 @@
 #include <cmath>
 #include <algorithm>
 #include <fstream>
+#include <tuple>
 
 using namespace std;
 
-
-struct Point {
-    int x, y;
+struct Point
+{
+    int id, x, y;
 };
 
-double calculateDistance(const Point& p1, const Point& p2) {
-    int dx = p1.x - p2.x;
-    int dy = p1.y - p2.y;
+double calculateDistance(const Point &p1, const Point &p2)
+{
+    double dx = p1.x - p2.x;
+    double dy = p1.y - p2.y;
     return std::sqrt(dx * dx + dy * dy);
 }
 
-vector<Point> greedyTSP(vector<Point> points) {
+tuple<vector<Point>, double> greedyTSP(vector<Point> points)
+{
     vector<Point> tour;
 
     // Start with the first point as the initial city
     tour.push_back(points[0]);
     points.erase(points.begin());
 
-    while (!points.empty()) {
+    double cost = 0;
+
+    while (!points.empty())
+    {
         Point currentCity = tour.back();
         double minDistance = calculateDistance(currentCity, points[0]);
         int closestCityIndex = 0;
 
         // Find the closest unvisited city
-        for (int i = 1; i < points.size(); i++) {
+        for (int i = 1; i < points.size(); i++)
+        {
             double distance = calculateDistance(currentCity, points[i]);
-            if (distance < minDistance) {
+            if (distance < minDistance)
+            {
                 minDistance = distance;
                 closestCityIndex = i;
             }
         }
+
+        cost += minDistance;
 
         // Move to the closest city
         tour.push_back(points[closestCityIndex]);
@@ -46,33 +56,59 @@ vector<Point> greedyTSP(vector<Point> points) {
     // Return to the starting city to complete the tour (would double with the couts at the end)
     // tour.push_back(tour[0]);
 
-    return tour;
+    cost += calculateDistance(tour.back(), tour[0]);
+
+    return {tour, cost};
 }
 
-int main() {
+int main(int argc, char *argsv[])
+{
     vector<Point> cities;
 
     // Read cities from a file
-    ifstream inputFile("plik.txt");
-    if (!inputFile.is_open()) {
+    string name="grupa7_1";
+    if (argc > 1)
+        name = argsv[1];
+    ifstream inputFile("../tests/"+name+".txt");
+    // ifstream inputFile("tests/olek123.txt");
+    if (!inputFile.is_open())
+    {
         cerr << "Failed to open the file." << endl;
         return 1;
     }
 
+    int n, s;
+    inputFile >> n;
+
     int x, y;
-    while (inputFile >> x >> y) {
-        Point point = {x,y};
+    for (int i = 1; i <= n; i++)
+    {
+        inputFile >> s >> x >> y;
+        Point point = {s, x, y};
         cities.push_back(point);
     }
     inputFile.close();
 
-    vector<Point> tour = greedyTSP(cities);
+    double cost;
+    vector<Point> tour;
 
-    cout << "trasa: ";
-    for (const Point& city : tour) {
-        cout << "(" << city.x << ", " << city.y << ") -> ";
-    }
-    cout << "(" << tour[0].x << ", " << tour[0].y << ")" << endl;
+    tie(tour, cost) = greedyTSP(cities);
+
+    // cout << "trasa po koordach: ";
+    // for (const Point &city : tour)
+    // {
+    //     cout << "(" << city.x << ", " << city.y << ") -> ";
+    // }
+    // cout << "(" << tour[0].x << ", " << tour[0].y << ")" << endl;
+
+    // cout << "trasa po id: ";
+    // for (const Point &city : tour)
+    // {
+    //     cout << "(" << city.id << ") -> ";
+    // }
+    // cout << "(" << tour[0].id << ")" << endl;
+
+    cout << "koszt: " << cost << endl;
 
     return 0;
 }
