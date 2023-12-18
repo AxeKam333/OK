fb94a18c895250e22597948949f13f9a77258c97
AxeKam333 <olek.kaminski5@gmail.com>
2023-12-18 09:24:19 +0100

    testowanie - bledny calculate distance

--- wyzarzanie.cpp
+++ wyzarzanie.cpp
@@ -63,6 +63,52 @@ vector<int> generateRandomTour(int n)
     return tour;
 }
 
+
+tuple<vector<int>, double> greedyTSP(vector<City> cities)
+{
+    vector<int> tour;
+
+    // Start with the first point as the initial city
+    tour.push_back(cities[0].name);
+    cities.erase(cities.begin());
+
+    double cost = 0;
+
+    while (!cities.empty())
+    {
+        int currentCity = tour.back();
+        double minDistance = calculateDistance(cities[currentCity], cities[0]);
+        int closestCityIndex = 0;
+
+        // Find the closest unvisited city
+        for (int i = 1; i < cities.size(); i++)
+        {
+            double distance = calculateDistance(cities[currentCity], cities[i]);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closestCityIndex = i;
+            }
+        }
+
+        cost += minDistance;
+
+        // Move to the closest city
+        tour.push_back(cities[closestCityIndex].name);
+        cities.erase(cities.begin() + closestCityIndex);
+    }
+
+    // Return to the starting city to complete the tour (would double with the couts at the end)
+    // tour.push_back(tour[0]);
+
+    cost += calculateDistance(cities[tour.back()], cities[tour[0]]);
+
+    cout << "Greedy cost: " << cost << endl;
+
+    return {tour, cost};
+}
+
+
 pair<vector<int>, double> swapCities(const vector<int> &tour, double currentDistance, const vector<City> &cities, int n, int swapNumber)
 {
     vector<int> newTour = tour;
@@ -77,33 +123,33 @@ pair<vector<int>, double> swapCities(const vector<int> &tour, double currentDist
     swaps[swapNumber - 1] = rand() % (n - 1) + 1;
     swap(newTour[swaps[swapNumber - 1]], newTour[swaps[0]]);
 
-    double newDistance = 0.0;
+    double distanceDifference = 0.0;
 
     for (int i = 0; i < swapNumber; i++)
     {
         if (swaps[i] == 0)
         {
-            newDistance -= calculateDistance(cities[tour[n - 1]], cities[tour[0]]);
-            newDistance += calculateDistance(cities[newTour[n - 1]], cities[newTour[0]]);
+            distanceDifference -= calculateDistance(cities[tour[n - 1]], cities[tour[0]]);
+            distanceDifference += calculateDistance(cities[newTour[n - 1]], cities[newTour[0]]);
         }
         else
         {
-            newDistance -= calculateDistance(cities[tour[swaps[i] - 1]], cities[tour[swaps[i]]]);
-            newDistance += calculateDistance(cities[newTour[swaps[i] - 1]], cities[newTour[swaps[i]]]);
+            distanceDifference -= calculateDistance(cities[tour[swaps[i] - 1]], cities[tour[swaps[i]]]);
+            distanceDifference += calculateDistance(cities[newTour[swaps[i] - 1]], cities[newTour[swaps[i]]]);
             if (swaps[i] + 1 == n)
             {
-                newDistance -= calculateDistance(cities[tour[swaps[i]]], cities[tour[0]]);
-                newDistance += calculateDistance(cities[newTour[swaps[i]]], cities[newTour[0]]);
+                distanceDifference -= calculateDistance(cities[tour[swaps[i]]], cities[tour[0]]);
+                distanceDifference += calculateDistance(cities[newTour[swaps[i]]], cities[newTour[0]]);
             }
             else
             {
-                newDistance -= calculateDistance(cities[tour[swaps[i]]], cities[tour[swaps[i] + 1]]);
-                newDistance += calculateDistance(cities[newTour[swaps[i]]], cities[newTour[swaps[i] + 1]]);
+                distanceDifference -= calculateDistance(cities[tour[swaps[i]]], cities[tour[swaps[i] + 1]]);
+                distanceDifference += calculateDistance(cities[newTour[swaps[i]]], cities[newTour[swaps[i] + 1]]);
             }
         }
     }
 
-    return make_pair(newTour, newDistance);
+    return make_pair(newTour, distanceDifference);
 }
 
 vector<int> simulatedAnnealing(const vector<City> &cities, double initialTemperature,
@@ -113,11 +159,15 @@ vector<int> simulatedAnnealing(const vector<City> &cities, double initialTempera
     srand(time(nullptr));
 
     int n = cities.size();
-    vector<int> currentTour = generateRandomTour(n);
+    vector<int> currentTour;
+    double currentDistance;
+    tie(currentTour,currentDistance) = greedyTSP(cities);
     vector<int> bestTour = currentTour;
+    
+    //  currentDistance = calculateTotalDistance(currentTour, cities);
+    double bestDistance = calculateTotalDistance(bestTour, cities);
 
-    double currentDistance = calculateTotalDistance(currentTour, cities);
-    double bestDistance = currentDistance;
+    cout<<bestDistance<<endl;
 
     for (int swapNumber = 2; swapNumber <= n / 2; swapNumber++)
     {
@@ -127,9 +177,15 @@ vector<int> simulatedAnnealing(const vector<City> &cities, double initialTempera
 
             vector<int> newTour;
             double newDistance;
+            double distanceDifference;
 
-            tie(newTour, newDistance) = swapCities(currentTour, currentDistance, cities, n, swapNumber);
+            tie(newTour, distanceDifference) = swapCities(currentTour, currentDistance, cities, n, swapNumber);
 
+
+            newDistance = calculateTotalDistance(newTour, cities);
+
+            // cout<<newDistance<<endl;
+            
             if (newDistance < currentDistance || (rand() / (RAND_MAX + 1.0)) < exp((currentDistance - newDistance) / temperature))
             {
                 currentTour = newTour;
@@ -138,6 +194,7 @@ vector<int> simulatedAnnealing(const vector<City> &cities, double initialTempera
 
             if (currentDistance < bestDistance)
             {
+                cout<<"NEW BEST"<<currentDistance<<" "<<bestDistance<<endl;
                 bestTour = currentTour;
                 bestDistance = currentDistance;
             }
@@ -193,12 +250,15 @@ int main(int argc, char *argsv[])
 
     double DistanceTable[Maxsize][Maxsize] = {};
     calculateDistanceTable(cities, DistanceTable, numCities);
-
+    
     double initialTemperature = 100000.0;
     double coolingRate = 0.00003;
     int maxIterations = 100000;
 
     vector<int> bestTour = simulatedAnnealing(cities, initialTemperature, coolingRate, maxIterations);
+    
+
+   
 
     cout << "Best Tour: ";
     for (int cityIndex : bestTour)
